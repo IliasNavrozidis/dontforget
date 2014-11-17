e69dbf77966891c4b8ea5c6b2a6cab0a1cdd6242
stagiaire <stagiaire@Mexico7-PC>
2014-11-17 17:34:31 +0100

    ok

--- src/com/formation/dontforget/MainActivity.java
+++ src/com/formation/dontforget/MainActivity.java
@@ -9,15 +9,18 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.*;
 
 import com.formation.dontforget.db.TaskContract;
 import com.formation.dontforget.db.TaskDBHelper;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends ListActivity{
 	private ListAdapter listAdapter;
 	private TaskDBHelper helper;
 
@@ -25,77 +28,113 @@ public class MainActivity extends ListActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		updateUI();
 		
-		//Set background image
-		View view = getWindow().getDecorView(); 
-		int orientation = getResources().getConfiguration().orientation; 
-		if (Configuration.ORIENTATION_LANDSCAPE == orientation) { 
-		           view.setBackgroundResource (R.drawable.task); 
-		} else { 
-		           view.setBackgroundResource (R.drawable.task); 
-		} 
+		ListView lst = (ListView)findViewById(R.id.taskTextView);
+		registerForContextMenu(lst);
+
+		updateUI();
+		// Set background image
+		View view = getWindow().getDecorView();
+		int orientation = getResources().getConfiguration().orientation;
+		if (Configuration.ORIENTATION_LANDSCAPE == orientation) {
+			view.setBackgroundResource(R.drawable.task);
+		} else {
+			view.setBackgroundResource(R.drawable.task);
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.menu,menu);
+		getMenuInflater().inflate(R.menu.menu, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-			case R.id.action_add_task:
-				AlertDialog.Builder builder = new AlertDialog.Builder(this);
-				builder.setTitle("Add a task");
-				builder.setMessage("What do you want to do?");
-				final EditText inputField = new EditText(this);
-				builder.setView(inputField);
-				builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialogInterface, int i) {
-						String task = inputField.getText().toString();
+		case R.id.action_add_task:
 
-						helper = new TaskDBHelper(MainActivity.this);
-						SQLiteDatabase db = helper.getWritableDatabase();
-						ContentValues values = new ContentValues();
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-						values.clear();
-						values.put(TaskContract.Columns.TASK,task);
+			builder.setTitle("Add a task");
 
-						db.insertWithOnConflict(TaskContract.TABLE,null,values,SQLiteDatabase.CONFLICT_IGNORE);
-						updateUI();
-					}
-				});
+			builder.setMessage("What do you want to do?");
+			final EditText inputField = new EditText(this);
 
-				builder.setNegativeButton("Cancel",null);
+			builder.setView(inputField);
 
-				builder.create().show();
-				return true;
+			builder.setPositiveButton("Add",
+					new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialogInterface,
+								int i) {
+							String task = inputField.getText().toString();
 
-			default:
-				return false;
+							helper = new TaskDBHelper(MainActivity.this);
+							SQLiteDatabase db = helper.getWritableDatabase();
+							ContentValues values = new ContentValues();
+
+							values.clear();
+							values.put(TaskContract.Columns.TASK, task);
+
+							db.insertWithOnConflict(TaskContract.TABLE, null,
+									values, SQLiteDatabase.CONFLICT_IGNORE);
+							updateUI();
+						}
+					});
+
+			builder.setNegativeButton("Cancel", null);
+
+			builder.create().show();
+			return true;
+
+		default:
+			return false;
 		}
 	}
 
 	private void updateUI() {
 		helper = new TaskDBHelper(MainActivity.this);
 		SQLiteDatabase sqlDB = helper.getReadableDatabase();
-		Cursor cursor = sqlDB.query(TaskContract.TABLE,
-				new String[]{TaskContract.Columns._ID, TaskContract.Columns.TASK},
-				null, null, null, null, null);
+		Cursor cursor = sqlDB.query(TaskContract.TABLE, new String[] {
+				TaskContract.Columns._ID, TaskContract.Columns.TASK }, null,
+				null, null, null, null);
 
-		listAdapter = new SimpleCursorAdapter(
-				this,
-				R.layout.task_view,
-				cursor,
-				new String[]{TaskContract.Columns.TASK},
-				new int[]{R.id.taskTextView},
-				0
-		);
+		listAdapter = new SimpleCursorAdapter(this, R.layout.task_view, cursor,
+				new String[] { TaskContract.Columns.TASK },
+				new int[] { R.id.taskTextView }, 0);
 
 		this.setListAdapter(listAdapter);
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		
+		switch (v.getId()) {
+		case R.id.taskTextView:
+			MenuInflater inflater = getMenuInflater();
+			inflater.inflate(R.menu.delete_menu, menu);
+			break;
+		}
+	}
+
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.delete:
+			Toast.makeText(this, "Delete button", Toast.LENGTH_SHORT).show();
+			return true;
+		case R.id.update:
+			Toast.makeText(this, "Update Button", Toast.LENGTH_SHORT).show();
+			return true;
+		
+		default:
+			return true;
+		}
+		
 	}
 
 	public void onDoneButtonClick(View view) {
@@ -104,10 +143,7 @@ public class MainActivity extends ListActivity {
 		String task = taskTextView.getText().toString();
 
 		String sql = String.format("DELETE FROM %s WHERE %s = '%s'",
-						TaskContract.TABLE,
-						TaskContract.Columns.TASK,
-						task);
-
+				TaskContract.TABLE, TaskContract.Columns.TASK, task);
 
 		helper = new TaskDBHelper(MainActivity.this);
 		SQLiteDatabase sqlDB = helper.getWritableDatabase();
